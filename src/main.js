0d2fdd1bdac635f0e83bb2f52f35a0fe31051ac8
Michael Babik <iviichae7@gmail.com>
2023-07-13 23:15:59 +0100

    Added the navbar, It needs to be fixed for mobile and medium size screen, the call to action also needs to be fixed, its unaligned for smaller screen, possibly shrink it create a container

---
+++ src/main.js
@@ -0,0 +1,5 @@
+document.getElementById('mobile-nav').addEventListener('click', function () {
+    document.getElementById('mobile-menu').classList.toggle('hidden');
+  });
+
+const video = document.getElementById('myVideo').playbackRate = 0.8;

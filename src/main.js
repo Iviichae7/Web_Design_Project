56bdfb8fdb17c261a4d9e957f31004bbc4647a21
Michael Babik <iviichae7@gmail.com>
2023-07-15 22:18:53 +0100

    Fixed few issues with responsivness. Added footer for now.

--- src/main.js
+++ src/main.js
@@ -1,5 +1,20 @@
+// Video speed
+const video = document.getElementById('myVideo').playbackRate = 0.8;
+
+// function for mobile menu
 document.getElementById('mobile-nav').addEventListener('click', function () {
     document.getElementById('mobile-menu').classList.toggle('hidden');
   });
 
-const video = document.getElementById('myVideo').playbackRate = 0.8;
+// function for cta to hide when mobile menu open
+const toggleCTA = () => {
+  let cta = document.getElementById('cta');
+
+  if (cta.style.display === 'none') {
+      cta.style.display = 'block';
+  } else {
+      cta.style.display = 'none';
+  }
+};
+document.getElementById('mobile-nav').addEventListener('click', toggleCTA);
+

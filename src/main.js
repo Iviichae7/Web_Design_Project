f94a5077424196e39ed70aaec6ec4261364f185b
iviichae7 <iviichae7@gmail.com>
2023-07-28 01:00:21 +0100

    Customised nav links and other minor tweaks

--- src/main.js
+++ src/main.js
@@ -1,20 +1,34 @@
 // Video speed
 const video = document.getElementById('myVideo').playbackRate = 0.8;
 
+// Selects all nav links
+const navLinks = document.querySelectorAll(".md\\:flex .text-xl");
+
 // function for mobile menu
 document.getElementById('mobile-nav').addEventListener('click', function () {
-    document.getElementById('mobile-menu').classList.toggle('hidden');
-  });
+  document.getElementById('mobile-menu').classList.toggle('hidden');
+});
 
 // function for cta to hide when mobile menu open
 const toggleCTA = () => {
   let cta = document.getElementById('cta');
 
   if (cta.style.display === 'none') {
-      cta.style.display = 'block';
+    cta.style.display = 'block';
   } else {
-      cta.style.display = 'none';
+    cta.style.display = 'none';
   }
 };
 document.getElementById('mobile-nav').addEventListener('click', toggleCTA);
 
+// Updates links on click
+navLinks.forEach(link => {
+  link.addEventListener('click', function (e) {
+
+    // removes the class from all links in nav
+    navLinks.forEach(link => link.classList.remove('active-link'));
+
+    // When click adds the class
+    this.classList.add('active-link');
+  })
+})

176d6c2314f346c5001cbac8e381b852c1147bee
Breda Logan <bredalogan@hotmail.com>
2023-08-01 22:43:15 +0100

    committCanges

---
+++ src/galleryScript.js
@@ -0,0 +1,8 @@
+function showText(text) {
+
+    // Get the HTML element to display the text
+    var choiceText = document.getElementById("choiceText");
+
+    // Display the text
+    choiceText.textContent = text;
+    }

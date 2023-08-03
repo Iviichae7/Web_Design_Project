b02a77cfd45b5265acbdef71be3658580d801f02
Michael Babik <iviichae7@gmail.com>
2023-08-03 18:25:58 +0100

    preparing for deployment

--- src/galleryScript.js
+++
@@ -1,8 +0,0 @@
-function showText(text) {
-
-    // Get the HTML element to display the text
-    var choiceText = document.getElementById("choiceText");
-
-    // Display the text
-    choiceText.textContent = text;
-    }

5ba3fcf7f32eb78a63f200fd1ff3cae2b5f6290a
Michael Babik <iviichae7@gmail.com>
2023-08-04 19:34:16 +0100

    seo, optimisation, buttons

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,14 @@
 module.exports = {
   purge: ['./*.html', './src/**/*.{js,css}'],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        'normal': ['Inconsolata', 'sans-serif'],
+        'heading': ['Inconsolata', 'sans-serif'],
+        'accent': ['Inconsolata', 'sans-serif'],
+      },
+    },
   },
+  variants: {},
   plugins: [],
 }
